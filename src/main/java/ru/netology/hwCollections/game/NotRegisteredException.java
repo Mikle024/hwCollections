83263a8fcc3ce1daef21768f1712524ef2790589
Mikhail <mikle0240@gmail.com>
2023-11-02 17:19:25 +0300

    Add NotRegisteredException.java

---
+++ src/main/java/ru/netology/hwCollections/game/NotRegisteredException.java
@@ -0,0 +1,7 @@
+package ru.netology.hwCollections.game;
+
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException(String msg) {
+        super(msg);
+    }
+}
